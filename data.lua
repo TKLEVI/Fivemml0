4c8adf17b25dbf98ba9f762da6f7ac6925b428c0
tdhialdowahuklk <151048161+TKLEVI@users.noreply.github.com>
2026-08-09 11:53:04 +0200

    Create data.lua

---
+++ data.lua
@@ -0,0 +1,16 @@
+Data = {
+    [1] = {
+        id = 1,
+        type = "Flitspaal",
+        location = "San Canzian d'Isonzo",
+        coords = vec3(45.807718, 13.441812, 0.0),
+        speed = 50
+    },
+    [2] = {
+        id = 2,
+        type = "Trajectcontrole",
+        location = "A2 Amsterdam",
+        coords = vec3(52.3105, 4.9421, 0.0),
+        speed = 100
+    }
+}
